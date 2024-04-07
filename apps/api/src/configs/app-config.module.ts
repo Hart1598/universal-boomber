70526862733bc1f0ee0fb7c4aa14df80b65e00f6
YaroslavKharchenko0 <yaroslav.harchenko@voypost.com>
2024-04-07 17:30:26 +0300

    feat: add RMQ module and configuration

--- apps/api/src/configs/app-config.module.ts
+++ apps/api/src/configs/app-config.module.ts
@@ -3,13 +3,19 @@ import { ConfigModule } from '@nestjs/config';
 import { getConfigModuleSettings } from './get-config-module.settings';
 import { LoggerModule } from '@app/logger';
 import { SERVICE_NAME } from '../constants';
+import { RMQModule } from 'nestjs-rmq';
+import { getRmqConfigSettings } from './get-rmq-config.settings';
 
 @Module({})
 export class AppConfigModule {
   static async forRoot() {
     return {
       module: AppConfigModule,
-      imports: [ConfigModule.forRoot(getConfigModuleSettings()), LoggerModule.forRootAsync(SERVICE_NAME)],
+      imports: [
+        ConfigModule.forRoot(getConfigModuleSettings()),
+        LoggerModule.forRootAsync(SERVICE_NAME),
+        RMQModule.forRootAsync(getRmqConfigSettings('endpoint-registry'))
+      ],
       controllers: [],
       providers: [],
     };
